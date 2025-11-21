07cc8070c0996bc968b8e8b5e7e9d7946aae073b
SwapnilMk <mswapnil218@gmail.com>
2025-11-21 22:15:53 +0530

    Fix Render build: install missing types and ignore dist

--- server/dist/config/cors.js
+++ server/dist/config/cors.js
@@ -4,18 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsOptions = void 0;
-const config_1 = __importDefault(require("./config"));
 const logger_1 = __importDefault(require("./logger"));
 // allowed Origin URLs for CORS
 const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "https://real-estate-management-web.vercel.app/",
-    config_1.default.FRONTEND_URL,
+    "*",
+    // "http://localhost:5173",
+    // "http://localhost:5174",
+    // "https://real-estate-management-web.vercel.app/",
+    // config.FRONTEND_URL,
 ];
 const corsOptions = {
     origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
+        if (!origin ||
+            allowedOrigins.includes("*") ||
+            allowedOrigins.includes(origin)) {
             callback(null, true);
         }
         else {
